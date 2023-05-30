4367715b6d39e02e9eec5f09877ba1d4537733ed
zigakk03 <zigakk03@gmail.com>
2023-05-30 09:09:56 +0200

    Not funcional navbar

---
+++ src/layouts/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+<nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid d-flex justify-content-between">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Settings</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../layouts/Navbar'
+
+function Home() {
+    return (
+        <div>
+          <Navbar />
+          <h1>Welcome to the Home Page</h1>
+          <p>This is the home page of your app.</p>
+        </div>
+    );
+}
+
+export default Home
