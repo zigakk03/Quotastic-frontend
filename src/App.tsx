fea53f268c47879fbfcc497c0bcb13f0eab4c8dd
zigakk03 <zigakk03@gmail.com>
2023-05-30 16:56:16 +0200

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
+import Page404 from './pages/Page404'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import UserPage from './pages/UserPage'
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/user' element={<UserPage />} />
+      <Route path='*' element={<Page404 />} />
+    </Routes>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter} from 'react-router-dom'
 
 
 const root = ReactDOM.createRoot(
@@ -9,6 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
