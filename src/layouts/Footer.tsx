125cf7043002cd75c1b637967274cc8b9e36406e
zigakk03 <zigakk03@gmail.com>
2023-05-31 12:09:24 +0200

    Footer

---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div className='container-fluid d-flex justify-content-between align-items-center text-white px-5' style={{height: 56, backgroundColor: '#DE8667'}}>
+        <img src={require('../assets/Logo_icon.png')} alt="logo" />
+        <p className='my-auto'>All Rights Reserved | skillupmentor.com</p>
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Footer from '../layouts/Footer'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      Home
+      <Footer />
+    </div>
   )
 }
 
