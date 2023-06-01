5e3699bf558d17365c9e4d3ea02b07a7ff7f5c84
zigakk03 <zigakk03@gmail.com>
2023-06-01 10:17:49 +0200

    Headers and typography

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,13 @@ import Footer from '../layouts/Footer'
 function Home() {
   return (
     <div>
-      Home
+      <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti, corrupti!</h1>
+      <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti, corrupti!</h2>
+      <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti, corrupti!</h3>
+      <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti, corrupti!</h4>
+      <h5>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti, corrupti!</h5>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. <b>Dolorem vero</b> quae architecto pariatur asperiores provident fugiat delectus id dolorum distinctio.</p>
+      <p className="small">Lorem ipsum dolor sit amet.</p>
       <Footer />
     </div>
   )
